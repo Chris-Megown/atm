5fa0f15641201dc5de2afc65cbbd905ee4dd49b9
Christopher A Megown <megown1204@gmail.com>
2017-09-05 18:49:37 -0400

    first time now?

---
+++ src/atm/Atm.java
@@ -0,0 +1,19 @@
+package atm;
+
+public class Atm {
+	
+	//an instance variable of type String called requiredPin that holds the PIN required to gain access.
+
+	String requiredPin;
+	
+	//a method with the signature boolean allowAccess(String enteredPin) that checks to see whether a PIN the user entered is valid.
+
+	boolean allowAccess(String enteredPin) {
+		return false;
+	}
+	
+	
+
+	
+	
+}
---
+++ src/atm/AtmClientApp.java
@@ -0,0 +1,19 @@
+package atm;
+import java.util.Scanner;
+
+public class AtmClientApp {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		//create an instance of ATM.
+		Atm Boom = new Atm();
+		
+		System.out.println("Welcome to Bank World!");
+		System.out.println("Please enter your PIN: ");
+		
+
+
+	}
+
+}
